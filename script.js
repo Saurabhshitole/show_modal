9bb2835f0b34206c91a4ccbf1c1dcc22a4f1a8e8
Saurabhshitole <65207891+Saurabhshitole@users.noreply.github.com>
2021-07-26 10:30:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnclosemodal = document.querySelector('.close-modal');
+const btnsopenmodal = document.querySelectorAll('.show-modal');
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsopenmodal.length; i++)
+  btnsopenmodal[i].addEventListener('click', openModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnclosemodal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
